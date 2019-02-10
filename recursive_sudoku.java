a66929342999796bbc772dda7f7c058bd57ea58a
Vincent Lobo <vincelobo4299@gmail.com>
2019-02-09 20:17:16 -0500

    Update recursive_sudoku.java

--- recursive_sudoku.java
+++ recursive_sudoku.java
@@ -1,7 +1,8 @@
 class Solver {
     private Square[][] enter;
     private boolean done;
-    
+    private int [][] temptest;
+
     public static void main(String[] args) {
         new Solver();
     }
@@ -13,8 +14,25 @@ class Solver {
                 enter[x][p]= new Square();
             }
         }
+        //TESTING ARRAYS
+        //int [][] temptest = {{0,1,5,4,6,9,7,3,8},{4,8,7,1,3,5,6,2,9},{6,9,3,8,7,2,4,1,5},{8,6,2,3,4,1,5,9,7},{5,4,9,2,8,7,3,6,1},{7,3,1,9,5,6,2,8,4},{9,2,4,5,1,3,8,7,6},{1,7,8,6,2,4,9,5,3},{3,5,6,7,9,8,1,4,2}};
+        int [][] temptest = {{2,0,0,3,0,0,0,0,0},{8,0,4,0,6,2,0,0,3},{0,1,3,8,0,0,2,0,0},{0,0,0,0,2,0,3,9,0},{5,0,7,0,0,0,6,2,1},{0,3,2,0,0,6,0,0,0,},{0,2,0,0,0,9,1,4,0},{6,0,1,2,5,0,8,0,9},{0,0,0,0,0,1,0,0,2}};
+
+        for(int t=0;t<9;t++)
+        {
+            for(int h=0;h<9;h++)
+            {
+                if(temptest[t][h]!=0)
+                {
+                    enter[t][h].finnum=temptest[t][h];
+                    enter[t][h].fin=true;
+                }
+            }
+        }
+
+        //enter = inputGui();
+        //end testing stuff
         done = false;
-        enter = inputGui();
         recursion(0,0);
         new TableGui(enter);
     }
@@ -30,20 +48,18 @@ class Solver {
                 System.out.println("\nTrying "+(trying+1)+" at ("+x+","+y+").   ");
                 if(colcheck(x,trying+1)&&rowcheck(y,trying+1)&&squarecheck(x,y,trying+1)) {
                     enter[x][y].finnum = trying+1;
-                if(x==8&&y==8) {
-                    done = true;
-                    return;
-                }
-                else if(y==8)
-                    recursion(x+1,0);
-                else
-                    recursion(x,y+1);
-                if(done)
-                    break;
+                    if(x==8&&y==8)
+                        done = true;
+                    else if(y==8)
+                        recursion(x+1,0);
+                    else
+                        recursion(x,y+1);
+                    if(done)
+                        break;
                 }
             }
         }
-        if(done)
+        if(enter[x][y].fin&&x==8&&y==8)
             return;
         else if(x!=8&&y==8)
             recursion(x+1,0);
@@ -53,7 +69,7 @@ class Solver {
 
     private boolean colcheck (int ex, int maybe) {
         for(int r=0;r<9;r++) {
-            if(enter[ex][r].fin&&enter[ex][r].finnum == maybe) {
+            if(enter[ex][r].finnum == maybe) {
                 System.out.print("Found another "+maybe+" in the column.");
                 return false;
             }
@@ -63,7 +79,7 @@ class Solver {
 
     private boolean rowcheck (int why, int maybe) {
         for(int r=0;r<9;r++) {
-            if(enter[r][why].fin&&enter[r][why].finnum == maybe) {
+            if(enter[r][why].finnum == maybe) {
                 System.out.print("Found another "+maybe+" in the row.");
                 return false;
             }
@@ -77,7 +93,7 @@ class Solver {
 
         for(int r=(3*a);r<(3*a+2);r++) {
             for(int s=(3*b);s<(3*b+2);s++) {
-                if(enter[r][s].fin&&enter[r][s].finnum == maybe) {
+                if(enter[r][s].finnum == maybe) {
                     System.out.print("Found another "+maybe+" in the square.");
                     return false;
                 }
