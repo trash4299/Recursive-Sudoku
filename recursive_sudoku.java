809fdf679734768e3990355a48a75357f8bf53ef
Vincent Lobo <vincelobo4299@gmail.com>
2019-02-20 11:19:00 -0500

    Update recursive_sudoku.java

--- recursive_sudoku.java
+++ recursive_sudoku.java
@@ -1,14 +1,18 @@
 class Solver {
     private Square[][] enter;
-    private boolean done;
     private int [][] temptest;
+    private boolean done;
+    public int x,y;
 
     public static void main(String[] args) {
         new Solver();
     }
 
     private Solver() {
+        done = false;
         enter = new Square[9][9];
+        x = 0;
+        y = 0;
         for (int x=0;x<9;x++) {
             for(int p=0;p<9;p++) {
                 enter[x][p]= new Square();
@@ -32,8 +36,7 @@ class Solver {
 
         //enter = inputGui();
         //end testing stuff
-        done = false;
-        recursion(0,0);
+        recursion(x,y);
         new TableGui(enter);
     }
 
@@ -41,62 +44,59 @@ class Solver {
         TableGui beginning = new TableGui();
         return beginning.give();
     }
-    
+
     private void recursion  (int x, int y) {
-        if(!done&&!enter[x][y].fin) {
+        if(!enter[x][y].fin) {
             for(int trying=0;trying<9;trying++) {
-                System.out.println("\nTrying "+(trying+1)+" at ("+x+","+y+").   ");
-                if(colcheck(x,trying+1)&&rowcheck(y,trying+1)&&squarecheck(x,y,trying+1)) {
+                System.out.print("Trying "+(trying+1)+" at ("+(x+1)+","+(y+1)+"). ");
+                if(check(x,y,trying+1)) {
                     enter[x][y].finnum = trying+1;
-                    if(x==8&&y==8)
+                    if(x==8&&y==8) {
                         done = true;
+                        return;
+                    }
                     else if(y==8)
                         recursion(x+1,0);
                     else
                         recursion(x,y+1);
-		    
-                    if(done)
-                        break;
-		    enter[x][y].finnum = 0;
+                    if(!done)
+                        enter[x][y].finnum = 0;
+                    else
+                        return;
                 }
             }
         }
-        if((done||enter[x][y].fin)&&x==8&&y==8)
-            return;
-        else if(x!=8&&y==8)
-            recursion(x+1,0);
-        else   //x < 8 and y < 8
-            recursion(x,y+1);
-    }
-
-    private boolean colcheck (int ex, int maybe) {
-        for(int r=0;r<9;r++) {
-            if(enter[ex][r].finnum == maybe) {
-                System.out.print("Found another "+maybe+" in the column.");
-                return false;
-            }
+        else {
+            if (x == 8 && y == 8) {
+                done = true;
+                return;
+            } else if (y == 8)
+                recursion(x + 1, 0);
+            else   //x < 8 and y < 8
+                recursion(x, y + 1);
         }
-        return true;
     }
 
-    private boolean rowcheck (int why, int maybe) {
-        for(int r=0;r<9;r++) {
-            if(enter[r][why].finnum == maybe) {
-                System.out.print("Found another "+maybe+" in the row.");
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean squarecheck(int ex, int why, int maybe) {
+    private boolean check (int ex, int why, int maybe) {
         int a = ex/3;
         int b = why/3;
 
-        for(int r=(3*a);r<(3*a+2);r++) {
-            for(int s=(3*b);s<(3*b+2);s++) {
-                if(enter[r][s].finnum == maybe) {
-                    System.out.print("Found another "+maybe+" in the square.");
+        for(int r=0;r<9;r++) {
+            if(r!=y&&enter[ex][r].finnum == maybe) {
+                System.out.println("Found another "+maybe+" in the row at ("+(ex+1)+","+(r+1)+").");
+                return false;
+            }
+        }
+        for(int r=0;r<9;r++) {
+            if(r!=x&&enter[r][why].finnum == maybe) {
+                System.out.println("Found another "+maybe+" in the column at("+(r+1)+","+(why+1)+").");
+                return false;
+            }
+        }
+        for(int r=(3*a);r<(3*a+3);r++) {
+            for(int s=(3*b);s<(3*b+3);s++) {
+                if(!(a==ex&&b==why)&&enter[r][s].finnum == maybe) {
+                    System.out.println("Found another "+maybe+" in the square at ("+(r+1)+","+(s+1)+").");
                     return false;
                 }
             }
