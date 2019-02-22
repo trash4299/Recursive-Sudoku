114955eb2dbbff304fbc28c67b6e2054710150fb
Vincent Lobo <vincelobo4299@gmail.com>
2019-02-22 00:47:15 -0500

    Comments
    I removed the testing code, but I left the print statements in so people can break down the progress of the solver.

--- Recursive_Sudoku.java
+++ Recursive_Sudoku.java
@@ -18,24 +18,8 @@ class Solver {
                 enter[x][p]= new Square();
             }
         }
-        //TESTING ARRAYS
-        //int [][] temptest = {{0,1,5,4,6,9,7,3,8},{4,8,7,1,3,5,6,2,9},{6,9,3,8,7,2,4,1,5},{8,6,2,3,4,1,5,9,7},{5,4,9,2,8,7,3,6,1},{7,3,1,9,5,6,2,8,4},{9,2,4,5,1,3,8,7,6},{1,7,8,6,2,4,9,5,3},{3,5,6,7,9,8,1,4,2}};
-        int [][] temptest = {{2,0,0,3,0,0,0,0,0},{8,0,4,0,6,2,0,0,3},{0,1,3,8,0,0,2,0,0},{0,0,0,0,2,0,3,9,0},{5,0,7,0,0,0,6,2,1},{0,3,2,0,0,6,0,0,0,},{0,2,0,0,0,9,1,4,0},{6,0,1,2,5,0,8,0,9},{0,0,0,0,0,1,0,0,2}};
-
-        for(int t=0;t<9;t++)
-        {
-            for(int h=0;h<9;h++)
-            {
-                if(temptest[t][h]!=0)
-                {
-                    enter[t][h].finnum=temptest[t][h];
-                    enter[t][h].fin=true;
-                }
-            }
-        }
-
-        //enter = inputGui();
-        //end testing stuff
+        
+        enter = inputGui();
         recursion(x,y);
         new TableGui(enter);
     }
@@ -48,7 +32,7 @@ class Solver {
     private void recursion  (int x, int y) {
         if(!enter[x][y].fin) {
             for(int trying=0;trying<9;trying++) {
-                System.out.print("\nTrying "+(trying+1)+" at ("+(x+1)+","+(y+1)+"). ");
+                //System.out.print("\nTrying "+(trying+1)+" at ("+(x+1)+","+(y+1)+"). ");
                 if(check(x,y,trying+1)) {
                     enter[x][y].finnum = trying+1;
                     if(x==8&&y==8) {
@@ -72,7 +56,7 @@ class Solver {
                 return;
             } else if (y == 8)
                 recursion(x + 1, 0);
-            else   //x < 8 and y < 8
+            else
                 recursion(x, y + 1);
         }
     }
@@ -83,20 +67,20 @@ class Solver {
 
         for(int r=0;r<9;r++) {
             if(enter[ex][r].finnum == maybe) {
-                System.out.print("Found another "+maybe+" in the row at ("+(ex+1)+","+(r+1)+").");
+                //System.out.print("Found another "+maybe+" in the row at ("+(ex+1)+","+(r+1)+").");
                 return false;
             }
         }
         for(int r=0;r<9;r++) {
             if(enter[r][why].finnum == maybe) {
-                System.out.print("Found another "+maybe+" in the column at("+(r+1)+","+(why+1)+").");
+                //System.out.print("Found another "+maybe+" in the column at("+(r+1)+","+(why+1)+").");
                 return false;
             }
         }
         for(int r=(3*a);r<(3*a+3);r++) {
             for(int s=(3*b);s<(3*b+3);s++) {
                 if(enter[r][s].finnum == maybe) {
-                    System.out.print("Found another "+maybe+" in the square at ("+(r+1)+","+(s+1)+").");
+                    //System.out.print("Found another "+maybe+" in the square at ("+(r+1)+","+(s+1)+").");
                     return false;
                 }
             }
